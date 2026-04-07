238e7b027c26c102d58c1ccfa36591c4f52612e2
Nurbu <lamanurbu2@gmail.com>
2026-04-07 15:43:07 -0400

    Fixed sentences

--- src/main/java/com/pluralsight/Reciept.java
+++ src/main/java/com/pluralsight/Reciept.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
 public class Reciept {
+    public static void main(String[] args) {
+        String item = "Yo-yo";
+        double price = 2.99;
+        int quantity = 3;
+        System.out.println("I bought " + quantity + " " + item + " for the kids, total was " + price);
+    }
 }
--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
 public class VariableApp {
+    public static void main(String[] args) {
+        String favoriteColor = "Red";
+        short startYear = 2026;
+        char middleInitial = 'J';
+        boolean havePets = false;
+        String niceMessage = "Have a great day!";
+        byte daysInAWeek = 7;
+        float coffeePrice = 4.99f;
+        char favoriteLetter = 'N';
+        boolean isRaining = false;
+        System.out.println("My favorite color is " + favoriteColor);
+        System.out.println("I started YearUpUnited in " + startYear);
+        System.out.println("My middle initial letter is " + middleInitial);
+        System.out.println("I have pets? " + havePets);
+        System.out.println("Nice message: " + niceMessage);
+        System.out.println("There are exactly " + daysInAWeek + " Days in a week");
+        System.out.println("The price of the coffee is $" + coffeePrice);
+        System.out.println("My favorite letter is " + favoriteLetter);
+        System.out.println("Raining is occuring outside: " + isRaining);
+
+
+    }
 }
